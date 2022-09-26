7ee92cc224d66883a6d5bf0b72e0fa990149515b
Klasnov <2654403080@qq.com>
2022-09-26 12:36:56 +0000

    Get the inspiration of how to finish find.c

--- user/find.c
+++ user/find.c
@@ -1,13 +1,15 @@
-# include "kernel/types.h"
-# include "kernel/stat.h"
-# include "kernel/fs.h"
-# include "user.h"
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/fs.h"
+#include "user.h"
 
 void find(char *path, char *name);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // Verify the number of the arguments
-    if (argc != 3) {
+    if (argc != 3)
+    {
         printf("Find needs two arguments!\n");
         exit(0);
     }
@@ -15,16 +17,26 @@ int main(int argc, char *argv[]) {
     exit(0);
 }
 
-void find(char *path, char *name) {
+void find(char *path, char *name)
+{
     int fd;
     struct stat st;
     // Open the file
-    if ((fd = open(path, 0)) < 0) {
-        fprintf(2, "The file doesn't exist!\n");
+    if ((fd = open(path, 0)) < 0)
+    {
+        fprintf(2, "Failed to open %s!\n", path);
         return;
     }
     // Get the information of the file
-    if (fstat(fd, &st) < 0) {
-        
+    if (fstat(fd, &st) < 0)
+    {
+        fprintf(2, "Failed to get the information of %s!\n", path);
+        return;
     }
+    // Return if it's not a directory
+    if (st.type != T_DIR) {
+        fprintf(2, "There isn't a directory named %s!\n", path);
+        return;
+    }
+    
 }
