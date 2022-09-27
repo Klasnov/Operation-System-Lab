6d94956657e724df9ed43a846c7eda1a5e3b48f8
Klasnov <2654403080@qq.com>
2022-09-27 15:15:35 +0000

    Debug edition of xargs.c's split().

--- user/xargs.c
+++ user/xargs.c
@@ -2,7 +2,7 @@
 # include "user.h"
 # include "kernel/param.h"
 
-# define BUFSIZ 256
+# define BUFSIZ 128
 
 /*
 Split the input string into different arguments.
@@ -25,50 +25,63 @@ int main(int argc, char *argv[])
     char **argAdd;
     fd = dup(0);
     argAdd = (char **)malloc(sizeof(char *) * MAXARG - argc);
+    printf("Allocate the memory space successfully.\n");
     read(fd, buf, BUFSIZ);
     split(buf, argAdd, &n);
-    printf("Run successfully!\n");
     exit(0);
 }
 
 void split(char *args, char **argv, int *n)
 {
-    // Preset the data
+    /* Preset the data */
     char *sta, *end, *idx;
     int i, len;
     *n = 0;
     len = strlen(args) - 1;
     sta = args;
-    // Skip the start blank space
-    for (sta = args; sta < args + len && *sta == ' '; sta++);
-    if (sta >= args + len) argv = 0;
-    // Load in the arguments
+    /* Skip the start blank space */
+    for (sta = args; sta < args + len && *sta == ' ' && *sta != '\0'; sta++);
+    printf("Skipped the openning blank space.\n");
+    if (sta >= args + len)
+    {
+        printf("Start pointer find too many blank inputs or no input.\n");
+        argv = 0;
+    }
+    /* Load in the arguments */
     else
     {
         for (end = sta; end < args + len; )
         {
-            // Read the argument's content util the first blank space
-            for (; *end != ' ' && *end != '\0' && end < args + len; end++);
+            /* Read the argument's content util the first blank space */
+            printf("1: end - sat = %d\n", end - sta);
+            for (; *end != ' ' && *end != '\n'; end++);
+            end--;
+            printf("*end = %c\n", *end);
+            printf("2: end - sat = %d\n", end - sta);
+            printf("End pointer find the end of an argument.\n");
+            /* Allocate space */
+            printf("3: end - sat = %d\n", end - sta);
+            printf("*n = %d\n", *n);
+            argv[*n] = (char *)malloc(sizeof(char) * (end - sta + 2));
+            printf("Allocate the memory space successfully.\n");
+            /* Record the argument read in */
+            printf("Record the argument: ");
+            for (idx = sta, i = 0; idx <= end; idx++, i++)
+            {
+                argv[*n][i] = *idx;
+                printf("%c", *idx);
+            }
+            argv[*n][i] = '\0';
+            (*n)++;
+            printf("!\n");
+            /* Skip the blank space and reset the pointer */
+            end++;
+            for (; *end == ' ' && *end != '\n'; end++);
             if (end >= args + len)
             {
-                if (*n != 0) break;
-                else
-                {
-                    argv = 0;
-                    break;
-                }
+                printf("End pointer has scanned all the string.\n");
+                break;
             }
-            end--;
-            // Allocate space
-            argv[*n] = (char *)malloc(sizeof(char) * (end - sta + 2));
-            // Record the argument read in
-            for (idx = sta, i = 0; idx <= end; idx++, i++) argv[*n][i] = *idx;
-            argv[*n][i] = '\0';
-            n++;
-            // Skip the blank space and reset the pointer
-            end++;
-            for (; *end == ' ' && *end != '\0'; end++);
-            if (end >= args + len) break;
             sta = end;
         }
     }
