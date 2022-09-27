3578624e91c4c8e24e8bd3e631152962f13b897e
Klasnov <2654403080@qq.com>
2022-09-27 15:56:13 +0000

    Finished xargs.c

--- user/xargs.c
+++ user/xargs.c
@@ -15,19 +15,54 @@ void split(char *args, char **argv, int *n);
 
 int main(int argc, char *argv[])
 {
+    /* Verify the account of the arguments */
     if (argc < 2)
     {
         printf("Xargs needs at least two arguments!\n");
         exit(0);
     }
-    int fd, n;
+    /* Data preset */
+    int n, i, id, stat;
     char buf[BUFSIZ];
-    char **argAdd;
-    fd = dup(0);
-    argAdd = (char **)malloc(sizeof(char *) * MAXARG - argc);
-    printf("Allocate the memory space successfully.\n");
-    read(fd, buf, BUFSIZ);
-    split(buf, argAdd, &n);
+    char **argAdd, **exAg;
+    argAdd = (char **)malloc(sizeof(char *) * (MAXARG - argc));
+    /* Circle of reading the input and execute command */
+    while(1)
+    {
+        /* Parse the string input in */
+        if (!strcmp(gets(buf, BUFSIZ), ""))
+        {
+            break;
+        }
+        split(buf, argAdd, &n);
+        /* Create the arguments pointer of exec() */
+        exAg = (char **)malloc(sizeof(char *) * (n + argc));
+        for (i = 0; i < argc - 1; i++)
+        {
+            exAg[i] = argv[i + 1];
+        }
+        for (int j = 0; j < n; j++)
+        {
+            exAg[i++] = argAdd[j];
+        }
+        exAg[i] = 0;
+        /* Create the parent and child pocess to execute the command */
+        if ((id = fork()) < 0)
+        {
+            printf("Failed to create the child process!\n");
+        }
+        /* Child process execute the command */
+        if (id == 0)
+        {
+            exec(argv[1], exAg);
+            exit(0);
+        }
+        /* Parent process wait until child process exited */
+        else
+        {
+            wait(&stat);
+        }
+    }
     exit(0);
 }
 
@@ -41,7 +76,6 @@ void split(char *args, char **argv, int *n)
     sta = args;
     /* Skip the start blank space */
     for (sta = args; sta < args + len && *sta == ' ' && *sta != '\0'; sta++);
-    printf("Skipped the openning blank space.\n");
     if (sta >= args + len)
     {
         printf("Start pointer find too many blank inputs or no input.\n");
@@ -53,33 +87,22 @@ void split(char *args, char **argv, int *n)
         for (end = sta; end < args + len; )
         {
             /* Read the argument's content util the first blank space */
-            printf("1: end - sat = %d\n", end - sta);
             for (; *end != ' ' && *end != '\n'; end++);
             end--;
-            printf("*end = %c\n", *end);
-            printf("2: end - sat = %d\n", end - sta);
-            printf("End pointer find the end of an argument.\n");
             /* Allocate space */
-            printf("3: end - sat = %d\n", end - sta);
-            printf("*n = %d\n", *n);
             argv[*n] = (char *)malloc(sizeof(char) * (end - sta + 2));
-            printf("Allocate the memory space successfully.\n");
             /* Record the argument read in */
-            printf("Record the argument: ");
             for (idx = sta, i = 0; idx <= end; idx++, i++)
             {
                 argv[*n][i] = *idx;
-                printf("%c", *idx);
             }
             argv[*n][i] = '\0';
             (*n)++;
-            printf("!\n");
             /* Skip the blank space and reset the pointer */
             end++;
             for (; *end == ' ' && *end != '\n'; end++);
             if (end >= args + len)
             {
-                printf("End pointer has scanned all the string.\n");
                 break;
             }
             sta = end;
